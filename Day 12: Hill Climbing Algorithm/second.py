1f00c872e15c2a30f9ace5a0753e6008fd4ed1d5
Cristian Santos <csantosr@unal.edu.co>
2022-12-26 15:56:07 -0500

    Day 12 part 2

--- Day 12: Hill Climbing Algorithm/second.py
+++ Day 12: Hill Climbing Algorithm/second.py
@@ -1,6 +1,13 @@
-inputFile = open('./input.txt', 'r')
+from parseInput import parse_input
+from findPath import get_steps
 
-# Second Star
-for line in inputFile:
-    pass
-inputFile.close()
+_, end, cleanMap, starts = parse_input()
+
+paths = []
+
+for start in starts:
+    steps = get_steps(start, end, cleanMap)
+    if steps is not None:
+        paths.append(steps)
+
+print(min(paths))
